7b8822483ca59332967d665158cad9bef657360f
Gerrit Niezen <gerrit.niezen@gmail.com>
2018-11-15 12:08:14 +0000

    remove dev testing code

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
 const binding = require('node-gyp-build')(__dirname);
 const fs = require('fs');
 
-fs.readFile('./LZO/cgm-history-compressed.bin', (err, data) => {
-  if (err) throw err;
-  console.log('Encoded length:', data.length);
-  let ret = binding.decompress(data, 4096);
-  console.log('Decoded.');
-  fs.readFile('./LZO/cgm-history-uncompressed.bin', (err, uncompressed) => {
-    console.log('Compared:',ret.compare(uncompressed));
-  });
-});
-
 module.exports = binding;
